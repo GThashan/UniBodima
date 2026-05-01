2bf9843df2c5ed337601ee7e04fdaf6d72a7a498
Gamage Hashan <tharinduh20@gmail.com>
2026-05-01 12:27:04 +0530

    user register controller and mongodb conneting

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,33 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// REGISTER
+export const register = async (req, res) => {
+  try {
+    const { name, phone, password, role } = req.body;
+
+    const existingUser = await User.findOne({ phone });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      phone,
+      password: hashedPassword,
+      role
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
